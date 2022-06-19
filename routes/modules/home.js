5654100d0a95569bac9155ce2a94ac91b7cf21ba
singingw <op0408io1998@gmail.com>
2022-06-19 20:07:03 +0800

    增加登入後的首頁資料串接

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res, next) => {
+  try {
+    //找出使用者資料
+    const userId = req.user._id
+    const records = await Record.find({ userId }).lean()
+    //首頁顯示資料
+    const categories = await Category.find().lean()
+    let totalAmount = 0
+    records.forEach(record => {
+      //總金額
+      totalAmount += record.amount
+      //連接 categoryId
+      const categoryId = record.categoryId
+      record.icon = categories.filter(category => categoryId.equals(category._id))[0].icon
+      //取出時間
+      let createYear = record.date.getYear() + 1900
+      createYear = createYear.toString()
+      let createMonth = record.date.getMonth() + 1
+      createMonth = createMonth.toString()
+      let createDate = record.date.getDate().toString()
+      record.date = createYear + "/" + createMonth + "/" + createDate
+    })
+    return res.render('index', {
+      records: records,
+      categories: categories,
+      totalAmount,
+    })
+  } catch (error) {
+    next(new Error('some error'));
+  }
 })
 
 module.exports = router
