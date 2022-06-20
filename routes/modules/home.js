2c6ee970c57c1bd09f1b1ca7a45959fa312ed558
singingw <op0408io1998@gmail.com>
2022-06-20 23:41:24 +0800

    增加篩選功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -28,10 +28,45 @@ router.get('/', async (req, res, next) => {
     return res.render('index', {
       records: records,
       categories: categories,
-      totalAmount,
+      totalAmount
     })
   } catch (error) {
-    next(new Error('some error'));
+    next(new Error('some error'))
+  }
+})
+router.post('/', async (req, res, next) => {
+  try {
+    //找出使用者資料
+    const userId = req.user._id
+    const records = await Record.find({ userId }).lean()
+    //首頁顯示資料
+    const categories = await Category.find().lean()
+    const { categorySelect } = req.body
+    let filterRecord = records
+    let totalAmount = 0
+    //篩選類別
+    const categoryId = categories.filter(category => category.name === categorySelect)[0]._id
+    filterRecord = records.filter(record => record.categoryId.equals(categoryId))
+
+    filterRecord.forEach(record => {
+      record.icon = categories.filter(category => categoryId.equals(category._id))[0].icon
+      //取出時間
+      let createYear = record.date.getYear() + 1900
+      createYear = createYear.toString()
+      let createMonth = record.date.getMonth() + 1
+      createMonth = createMonth.toString()
+      let createDate = record.date.getDate().toString()
+      record.date = createYear + "/" + createMonth + "/" + createDate
+      totalAmount += record.amount
+    })
+    return res.render('index', {
+      records: filterRecord,
+      categories: categories,
+      totalAmount,
+      categorySelect
+    })
+  } catch (error) {
+    next(new Error('some error'))
   }
 })
 
