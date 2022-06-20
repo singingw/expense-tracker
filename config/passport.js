fc993a3a5fd2702be210d9159617992577fb2964
singingw <op0408io1998@gmail.com>
2022-06-20 20:53:35 +0800

    新增第三方登入功能

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 module.exports = app => {
   // 初始化 Passport 模組
@@ -25,6 +27,58 @@ module.exports = app => {
         })
       })
       .catch(err => done(err, false))
+  }))
+
+  //Facebook 驗證
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+      .catch(err => console.log(err))
+  }))
+
+  //Google 驗證
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+      .catch(err => console.log(err))
   }))
 
   // 設定序列化與反序列化
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
 const record = require('./modules/record')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/records', authenticator, record)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 // 匯出路由器
